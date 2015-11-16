68925e6e3fb283c2c6ca8df8365c8d52dd06f6b0
Thomas Conner <thomas@kinvey.com>
2015-11-16 11:40:37 -0500

    MLIBZ-634 Fix bug that prevented the sample from running on Windows.
    Signed-off-by: Thomas Conner <thomas@kinvey.com>

--- setup.js
+++ setup.js
@@ -1,16 +1,19 @@
-/*eslint-disable */
-
 'use strict';
 
 var prompt = require('prompt');
 var chalk = require('chalk');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
+var async = require('async');
+var inquirer = require('inquirer');
 var path = require('path');
 var spawn = require('child_process').spawn;
+var bower = require('bower');
 var configFile = path.join(__dirname, 'src/modules/config.js');
 
 console.log(chalk.cyan('This utility will walk you through setting up the Kinvey Ionic Starter App.'));
-console.log(chalk.cyan('Please refer to http://devcenter.kinvey.com/phonegap for more information. Defaults are shown next to the question.'))
+console.log(chalk.cyan('Please refer to http://devcenter.kinvey.com/phonegap for more information. Defaults are shown next to the question.'));
 console.log(chalk.cyan('Press ^C at any time to quite.'));
 console.log('');
 
@@ -26,17 +29,17 @@ prompt.get({
   properties: {
     appKey: {
       description: 'What is your app key?',
-      required: true
+      required: true,
     },
     appSecret: {
       description: 'What is your app secret?',
-      required: true
+      required: true,
     },
     apiHostname: {
       description: 'What is the API hostname?',
-      default: 'https://baas.kinvey.com'
-    }
-  }
+      default: 'https://baas.kinvey.com',
+    },
+  },
 }, function(err, result) {
   if (err) {
     console.log(chalk.red(err));
@@ -58,18 +61,75 @@ prompt.get({
       return;
     }
 
-    var setup = spawn('./setup.sh');
+    async.parallel([
+      function(callback) {
+        console.log('Removing plugins directory...\n');
+        rimraf(__dirname + '/plugins', callback);
+      },
+      function(callback) {
+        console.log('Removing platforms directory...\n');
+        rimraf(__dirname + '/platforms', callback);
+      },
+    ], function(err, results) {
+      if (err) {
+        console.log(chalk.red(err));
+        return;
+      }
 
-    setup.stdout.on('data', function(data) {
-      console.log(data.toString());
-    });
+      async.parallel([
+        function(callback) {
+          console.log('Creating plugins directory...\n');
+          mkdirp(__dirname + '/plugins', callback);
+        },
+        function(callback) {
+          console.log('Creating platforms directory...\n');
+          mkdirp(__dirname + '/platforms', callback);
+        },
+      ], function(err, results) {
+        if (err) {
+          console.log(chalk.red(err));
+          return;
+        }
 
-    setup.stderr.on('data', function(data) {
-      console.log(chalk.red(data));
-    });
+        async.parallel([
+          function(callback) {
+            console.log('Installing Bower components...\n');
+            bower.commands.install(null, { save: true }, { interactive: true })
+            .on('prompt', function(prompts, callback) {
+              inquirer.prompt(prompts, callback);
+            })
+            .on('end', function(installed) {
+              callback(null, installed);
+            })
+            .on('error', function(err) {
+              callback(err);
+            });
+          },
+          function(callback) {
+            console.log('Setting up Ionic...\n');
+            var setup = spawn('./node_modules/.bin/ionic', ['state', 'reset']);
 
-    setup.on('close', function(code) {
-      console.log('Done. Start the application by running `ionic run ios --device` or `ionic run android --device`.');
+            setup.stdout.on('data', function(data) {
+              console.log(data.toString());
+            });
+
+            setup.stderr.on('data', function(data) {
+              callback(data);
+            });
+
+            setup.on('close', function(code) {
+              callback();
+            });
+          },
+        ], function(err, results) {
+          if (err) {
+            console.log(chalk.red(err));
+            return;
+          }
+
+          console.log('Project is all setup. Start project but executing npm start.');
+        });
+      });
     });
   });
 });
